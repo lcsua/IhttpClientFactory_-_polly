cefb4bbba8e57487422811f3e19888435821df1a
lcsua <lucasmbolivar@gmail.com>
2020-04-29 00:05:50 -0300

    meli service

---
+++ WebApp/Core/Aplication/Interfaces/IMeliService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Core.Aplication.Interfaces
+{
+    public interface IMeliService
+    {
+        Task<ExpandoObject> GetMeliUser(long userId);
+    }
+}
---
+++ WebApp/Infrastructure/Services/MeliService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApp.Core.Aplication.Interfaces;
+
+namespace WebApp.Infrastructure.Services
+{
+    public class MeliService : IMeliService
+    {
+        public IHttpClientFactory _httpClientFactory { get; }
+        public MeliService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        
+        public async Task<ExpandoObject> GetMeliUser(long userId)
+        {
+            var httpClient = _httpClientFactory.CreateClient("MeliClient");
+            var meliUser = await httpClient.GetStringAsync($"users/{userId}");
+            return JsonConvert.DeserializeObject<ExpandoObject>(meliUser);
+        }
+    }
+}
