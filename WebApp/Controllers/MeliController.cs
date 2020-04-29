72b3067c72a657f5a23995e1b53fffe97e76ee49
lcsua <lucasmbolivar@gmail.com>
2020-04-29 00:06:12 -0300

    meli controller

---
+++ WebApp/Controllers/MeliController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Core.Aplication.Interfaces;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Produces("application/json",
+              "application/xml")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class MeliController : Controller
+    {
+        public IMeliService _meliService { get; }
+        public MeliController(IMeliService meliService)
+        {
+            _meliService = meliService;
+        }
+
+               
+        [HttpGet("{userId}/user")]      
+        public async Task<IActionResult> GetUser(long userId)
+        {
+            var user = await _meliService.GetMeliUser(userId);
+           return Ok(user);
+        }
+    }
+}
